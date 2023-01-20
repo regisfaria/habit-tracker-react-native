f3374e39cb84bb2d6088d5de0e2c0f77fa9885c8
regisfaria <regisprogramming@gmail.com>
2023-01-20 01:14:03 +0100

    improved home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
-import { DAY_SIZE } from "../components/HabitDay";
+import { Text, View, ScrollView } from "react-native";
 
+import { DAY_SIZE, HabitDays } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
 const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDateSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDateSize - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -21,6 +25,26 @@ export function Home() {
           </Text>
         ))}
       </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDays key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                key={i}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
