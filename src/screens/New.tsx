1286a93f649ca6dab94e6b6d6dec8a68973905ae
regisfaria <regisprogramming@gmail.com>
2023-01-22 11:20:35 +0100

    implemented 'New' component

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
 import { BackButton } from "../components/BackButton";
 
 export function New() {
@@ -6,6 +6,16 @@ export function New() {
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}>
         <BackButton />
+
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Create Habit
+        </Text>
+
+        <Text className="mt-6 text-white font-semibold text-base">
+          What you're committing to?
+        </Text>
+
+        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-violet-600" />
       </ScrollView>
     </View>
   );
